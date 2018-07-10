f2dc0c7957cce2d466e933bbd318643e500cfcbf
Ashley Minor <aminor@kipphouston.org>
2018-07-10 18:29:18 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,11 +1,11 @@
-def mothers_day
-	"Happy Mother's Day, Mom! I hope you have a great day!"
-end
+# def mothers_day
+# 	"Happy Mother's Day, Mom! I hope you have a great day!"
+# end
 
-## Part Two - Mother's Day with One Argument
-def mothers_day(name)
-	"Happy Mother's Day, #{name}! I hope you have a great day!"
-end
+# ## Part Two - Mother's Day with One Argument
+# def mothers_day(name)
+# 	"Happy Mother's Day, #{name}! I hope you have a great day!"
+# end
 
 ## Part Three - Mother's Day with One Optional Argument
 def mothers_day(name="Mom")
